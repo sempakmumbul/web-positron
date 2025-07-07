4ae9c23ee2c822f75edd32e2c472f0b746734381
havenobrain1 <avidmuhammad128@gmail.com>
2025-07-07 21:21:21 +0700

    Update tentang.blade.php

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -450,7 +450,7 @@
                         </div>
                     </div>
 
-        
+
 
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
                 <script>
